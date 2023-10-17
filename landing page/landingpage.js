de6f604d409a2c1a3afaaf95bb34778c42b7e2e6
Paruchuru venkata sai seshu mohith <144360254+mohith1530@users.noreply.github.com>
2023-10-17 23:43:39 +0530

    Add files via upload

---
+++ landing page/landingpage.js
@@ -0,0 +1,5 @@
+// JavaScript code can be added here to enhance functionality
+document.getElementById('signup-btn').addEventListener('click', function() {
+    // Add your signup logic here
+    alert('Sign up button clicked!');
+});
